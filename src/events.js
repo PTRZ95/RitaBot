0472d9b99bfeb90c09ff4ed005d5b26505e6340c
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-17 10:16:57 -0700

    Update events.js

--- src/events.js
+++ src/events.js
@@ -19,7 +19,7 @@ const joinMessage = require("./commands/info_commands/join");
 // ----------
 // Core Code
 // ----------
-const activities_list = ["ritabot.gg | !tr help", "translations | ritabot.gg"]; // creates an arraylist containing phrases you want your bot to switch through.
+const activities_list = ["ritabot.gg | !tr help", "translations | ritabot.gg"];
 
 exports.listen = function listen (client)
 {
