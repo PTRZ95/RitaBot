8330f6c5603790b450ae3e5c29348ea11794603e
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-17 10:14:40 -0700

    Update events.js

--- src/events.js
+++ src/events.js
@@ -24,12 +24,6 @@ const activities_list = [
         "translations | ritabot.gg"
         ]; // creates an arraylist containing phrases you want your bot to switch through.
 
-client.on('ready', () => {
-        setInterval(() => {
-            const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-            client.user.setActivity(activities_list[index], {type: 'WATCHING'}); // sets bot's activities to one of the phrases in the arraylist.
-        }, 10000); // Runs this every 10 seconds.
-    });
 exports.listen = function listen (client)
 {
 
