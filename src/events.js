928cef0e9726eddb963fb5f137648cf3cb7e8c43
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-17 10:30:32 -0700

    Update events.js

--- src/events.js
+++ src/events.js
@@ -20,13 +20,13 @@ const joinMessage = require("./commands/info_commands/join");
 // Core Code
 // ----------
 const activities_list = [
-   `ritabot.gg | !tr help`,
-   `for messages to translate | ritabot.gg`,
-   "messages to translate | ritabot.gg",
-   `!tr help commands | ritabot.gg`,
-   "translations | ritabot.gg",
-   `v.${config.version} | ritabot.gg`,
-   `!tr help modules | ritabot.gg`
+      `ritabot.gg | !tr help`,
+      `for messages to translate | ritabot.gg`,
+      "messages to translate | ritabot.gg",
+      `!tr help commands | ritabot.gg`,
+      "translations | ritabot.gg",
+      `v.${config.version} | ritabot.gg`,
+      `!tr help modules | ritabot.gg`
 ];
 
 exports.listen = function listen (client)
