2b1f50d37937d98870712d80ec070cb919064a97
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-17 10:11:37 -0700

    Update events.js

--- src/events.js
+++ src/events.js
@@ -19,7 +19,17 @@ const joinMessage = require("./commands/info_commands/join");
 // ----------
 // Core Code
 // ----------
+   const activities_list = [
+           "ritabot.gg | !tr help", 
+           "translations | ritabot.gg"
+           ]; // creates an arraylist containing phrases you want your bot to switch through.
 
+   client.on('ready', () => {
+           setInterval(() => {
+               const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+               client.user.setActivity(activities_list[index], {type: 'WATCHING'}); // sets bot's activities to one of the phrases in the arraylist.
+           }, 10000); // Runs this every 10 seconds.
+       });
 exports.listen = function listen (client)
 {
 
@@ -106,17 +116,7 @@ exports.listen = function listen (client)
 
       }
    );
-   const activities_list = [
-           "ritabot.gg | !tr help", 
-           "translations | ritabot.gg"
-           ]; // creates an arraylist containing phrases you want your bot to switch through.
 
-   client.on('ready', () => {
-           setInterval(() => {
-               const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-               client.user.setActivity(activities_list[index], {type: 'WATCHING'}); // sets bot's activities to one of the phrases in the arraylist.
-           }, 10000); // Runs this every 10 seconds.
-       });
    // -----------------
    // Recieved Message
    // -----------------
